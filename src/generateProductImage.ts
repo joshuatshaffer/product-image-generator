453d0b3b87dbef35ef7f8257aa8d048c0deaba47
Joshua Shaffer <joshua@joshuatshaffer.com>
2024-06-13 22:45:55 -0500

    Add matrix of textures and shapes.

--- src/generateProductImage.ts
+++ src/generateProductImage.ts
@@ -1,6 +1,6 @@
 import { randomElement } from "./randomElement";
 
-const textureNames = [
+export const textureNames = [
   "fabric",
   "grass",
   "concrete",
@@ -10,39 +10,103 @@ const textureNames = [
   "noodles",
   "flower",
   "bricks",
-];
+] as const;
 
-const shapes = [
-  /* HTML */ ` <circle cx="512" cy="512" r="512" fill="url(#pattern)" /> `,
-  /* HTML */ `
-    <rect x="10" y="10" width="1004" height="1004" fill="url(#pattern)" />
+export type TextureName = (typeof textureNames)[number];
+
+interface ShapeArgs {
+  patternId: string;
+}
+
+const shapes = {
+  ball: ({ patternId }: ShapeArgs) => /* HTML */ `
+    <circle cx="512" cy="512" r="502" fill="url(#${patternId})" />
   `,
-];
+  block: ({ patternId }: ShapeArgs) => /* HTML */ `
+    <rect x="10" y="10" width="1004" height="1004" fill="url(#${patternId})" />
+  `,
+  lamp: ({ patternId }: ShapeArgs) => /* HTML */ `
+    <path
+      style="fill:url(#${patternId});"
+      d="m 459.87859,535.12917 0.97872,-195.70649 86.00682,-0.52808 4.224,196.50824 z"
+    />
+    <path
+      style="fill:#000;fill-opacity:0.5;stroke:none;stroke-width:1.32274px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+      d="m 459.87859,535.12917 0.97872,-195.70649 86.00682,-0.52808 4.224,196.50824 z"
+    />
+    <path
+      style="fill:url(#${patternId});"
+      d="m 269.24462,77.597797 c 0,0 94.70712,-35.3119 234.19725,-35.091958 155.12677,0.244568 242.09066,39.806925 242.09066,39.806925 L 681.7571,242.31344 337.85282,260.47539 Z"
+    />
+    <path
+      style="fill:#000;fill-opacity:0.5;stroke:#000000;stroke-width:1.32274px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+      d="m 269.24462,77.597797 c 0,0 94.70712,-35.3119 234.19725,-35.091958 155.12677,0.244568 242.09066,39.806925 242.09066,39.806925 L 681.7571,242.31344 337.85282,260.47539 Z"
+    />
+    <path
+      style="fill:url(#${patternId});fill-opacity:1;stroke:#000000;stroke-width:1.32274px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+      d="m 339.89434,982.76812 337.6462,-0.89321 c 0,0 101.16676,-200.90867 103.03032,-293.03319 1.86361,-92.12453 -136.21895,-193.02533 -136.21895,-193.02533 l -265.81444,4.17916 c 0,0 -153.28007,62.4791 -156.1422,190.40387 -2.86212,127.92471 117.49907,292.3687 117.49907,292.3687 z"
+    />
+    <path
+      style="fill:url(#${patternId});fill-opacity:1;stroke:#000000;stroke-width:1.32274px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+      d="M 92.643384,430.80422 269.24462,77.597797 c 0,0 63.08889,49.191173 221.95138,48.573883 166.40266,-0.64659 254.33653,-43.858916 254.33653,-43.858916 L 955.68284,378.77927 c 0,0 -221.73812,88.35821 -437.71734,83.25162 C 301.98629,456.9243 92.643384,430.80422 92.643384,430.80422 Z"
+    />
+  `,
+};
+
+export type ShapeName = keyof typeof shapes;
+
+export const shapeNames = Object.keys(shapes) as ShapeName[];
+
+interface GenerateProductImageOptions {
+  textureName?: TextureName;
+  shapeName?: ShapeName;
+  width?: number;
+  height?: number;
+
+  /**
+   * Make the SVG usable as an image. See
+   * https://developer.mozilla.org/en-US/docs/Web/SVG/SVG_as_an_Image for
+   * details.
+   */
+  asImage?: boolean;
+}
 
 export function generateProductImage({
-  texture = randomElement(textureNames),
-} = {}) {
-  return /* HTML */ `<svg
-    xmlns="http://www.w3.org/2000/svg"
-    xmlns:xlink="http://www.w3.org/1999/xlink"
-    viewbox="0 0 1024 1024"
-  >
-    <defs>
-      <pattern
-        id="pattern"
-        width="1024"
-        height="1024"
-        patternUnits="userSpaceOnUse"
-      >
-        <image
-          href="textures/${texture}-diffuse.jpg"
-          x="0"
-          y="0"
+  textureName = randomElement(textureNames),
+  shapeName = randomElement(shapeNames),
+  width = 1024,
+  height = 1024,
+
+  asImage = false,
+}: GenerateProductImageOptions = {}) {
+  const patternId = Math.random().toString(36).substring(7);
+
+  return /* HTML */ `
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewbox="0 0 1024 1024"
+      width="${width}"
+      height="${height}"
+    >
+      <defs>
+        <pattern
+          id="${patternId}"
           width="1024"
           height="1024"
-        />
-      </pattern>
-    </defs>
-    ${randomElement(shapes)}
-  </svg>`;
+          patternUnits="userSpaceOnUse"
+        >
+          <!-- Fallback color when image is not loaded. -->
+          <rect x="0" y="0" width="1024" height="1024" fill="#ddd" />
+          <image
+            href="/textures/${textureName}-diffuse.jpg"
+            x="0"
+            y="0"
+            width="1024"
+            height="1024"
+          />
+        </pattern>
+      </defs>
+      ${shapes[shapeName]({ patternId })}
+    </svg>
+  `;
 }
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,99 @@
 import express from "express";
-import { textureNames } from "./textureNames";
-import { generateProductImage } from "./generateProductImage";
+import {
+  ShapeName,
+  TextureName,
+  generateProductImage,
+  shapeNames,
+  textureNames,
+} from "./generateProductImage";
+import { randomElement } from "./randomElement";
 
 const app = express();
 const port = 3000;
 
 app.use("/textures", express.static("textures"));
 
-app.get("/", (req, res, next) => {
-  res.type("svg").send(generateProductImage());
+app.get("/", (req, res) => {
+  res.type("html").send(
+    "<!DOCTYPE html>\n" +
+      /* HTML */ `
+        <html>
+          <body>
+            <table>
+              ${textureNames
+                .map(
+                  (textureName) => /* HTML */ `
+                    <tr>
+                      ${shapeNames
+                        .map((shapeName) => {
+                          const imageUrl = getImageUrl({
+                            shapeName,
+                            textureName,
+                          });
+
+                          return /* HTML */ `
+                            <td>
+                              <a href="${imageUrl}">
+                                ${generateProductImage({
+                                  shapeName,
+                                  textureName,
+                                  width: 128,
+                                  height: 128,
+                                })}
+                              </a>
+                            </td>
+                          `;
+                        })
+                        .join("\n")}
+                    </tr>
+                  `
+                )
+                .join("\n")}
+            </table>
+          </body>
+        </html>
+      `
+  );
+});
+
+app.get("/image", (req, res, next) => {
+  const textureName = textureNames.find((x) => x === req.query.textureName);
+  const shapeName = shapeNames.find((x) => x === req.query.shapeName);
+
+  if (textureName === undefined || shapeName === undefined) {
+    res.redirect(
+      getImageUrl({
+        textureName: textureName ?? randomElement(textureNames),
+        shapeName: shapeName ?? randomElement(shapeNames),
+      })
+    );
+    return;
+  }
+
+  res.type("svg").send(
+    generateProductImage({
+      textureName,
+      shapeName,
+    })
+  );
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}/`);
 });
+
+function getImageUrl({
+  shapeName,
+  textureName,
+}: {
+  shapeName: ShapeName;
+  textureName: TextureName;
+}) {
+  return (
+    "/image?" +
+    new URLSearchParams({
+      shapeName,
+      textureName,
+    })
+  );
+}
